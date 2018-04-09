5635ac540d4c37384ae2e457bdfcb1065ee83b73
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2018-04-09 17:50:10 -0500

    Allows override of testMatch

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -80,6 +80,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
     'resetModules',
     'snapshotSerializers',
     'watchPathIgnorePatterns',
+    'testMatch',
   ];
   if (overrides) {
     supportedKeys.forEach(key => {
